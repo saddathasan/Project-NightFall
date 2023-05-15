d2a54dd673b05f10b6499135bcc569c13c377317
saddathasan94@gmail.com <saddathasan94@gmail.com>
2023-05-15 19:14:51 +0600

    wrapper added

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@ const age = 20;
 </script>
 
 <template>
-<div>
-  <h2>my age is {{ age }}</h2>
+<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
 </div>
 </template>
 
